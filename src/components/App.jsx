db68b669d19facd54f30e57bd51f7614fbbb918e
Dmytro Stupak <stupakbf@gmail.com>
2022-09-07 18:31:46 +0300

    add Profile

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import Profile from './profile/Profile';
+import user from '../data/user';
+
+
+
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+    </div>
+  );
+};
